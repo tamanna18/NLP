befd244cf20992c883f4c721f60bca45b5d305f9
Tamanna <57901189+tamanna18@users.noreply.github.com>
2020-12-21 00:58:38 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+setup(
+    name = 'krippendorff_alpha',
+    version = '0.01',
+    author = "Tamanna",
+    author_email = "tam.tamanna18@gmail.com",
+    maintainer = "Tamanna",
+    maintainer_email = "tam.tamanna18@gmail.com",
+    description = "Python implementation of Krippendorff's alpha measure for interrated agreement",
+    license = "GNU General Public License v3 (GPLv3)",
+    url = "https://github.com/tamanna18/NLP",
+    py_modules = ["krippendorff_alpha"],
